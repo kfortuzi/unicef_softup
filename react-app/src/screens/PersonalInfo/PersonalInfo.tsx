d1fb67381dcd97d94776784039b67ac5a388f343
Emre KAS <emre.kas@softup.co>
2024-03-27 15:30:54 +0000

    Merged in feature/UA-131-fe-implement-profile-picture-with (pull request #52)
    UA-131 Added upload profile picture
    
    * feat: added upload profile picture
    
    
    Approved-by: Stivi Ndoni

--- react-app/src/screens/PersonalInfo/PersonalInfo.tsx
+++ react-app/src/screens/PersonalInfo/PersonalInfo.tsx
@@ -1,12 +1,15 @@
-import { Col, Image, Row } from 'antd';
+import { Col, Image, Row, Upload, UploadProps, message } from 'antd';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
+import useGetProfile from 'src/api/users/hooks/useGetProfile';
 import Button from 'src/components/common/Button/Button';
 import PersonalInfoDescription from 'src/components/profile/personalInfo/PersonalInfoDescription/PersonalInfoDescription';
 import PersonalInfoEditForm from 'src/components/profile/personalInfo/PersonalInfoEditForm/PersonalInfoEditForm';
+import config from 'src/config';
 import { Route } from 'src/router/enums';
+import { LocalStorageKey, getItem } from 'src/utils/storage';
 
 const PersonalInfo: React.FC = () => {
   const navigate = useNavigate();
@@ -14,6 +17,28 @@ const PersonalInfo: React.FC = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'profile.personalInfo' });
   const toggleIsEditForm = () => {
     setIsEditMode((currentIsEdit) => !currentIsEdit);
+  };
+
+  const { data: user, refetch } = useGetProfile();
+
+  const uploadProps: UploadProps = {
+    name: 'file',
+    action: `${config.API_BASE_URL}/user/uploadFile`,
+    headers: {
+      authorization: `Bearer ${getItem<string>(LocalStorageKey.USER_SESSION_TOKEN) || ''}`,
+    },
+    accept: 'image/*',
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        message.info(t('uploading'));
+      }
+      if (info.file.status === 'done') {
+        message.success(t('uploadSuccess'));
+        refetch();
+      } else if (info.file.status === 'error') {
+        message.error(t('uploadError'));
+      }
+    },
   };
 
   return (
@@ -28,19 +53,26 @@ const PersonalInfo: React.FC = () => {
       <Row
         justify="center"
         align="middle"
+        gutter={[16, 16]}
       >
-        <Col span={6}>
+        <Col
+          span={6}
+          style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}
+        >
           <Image
             preview={false}
             width="50%"
-            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            src={user?.profilePicture}
+            fallback="/src/assets/images/profile-placeholder.png"
           />
-          <Button
-            type="primary"
-            text="Change Photo"
-            onClick={() => navigate(Route.PERSONAL_INFO)}
-            style={{ marginTop: '20px' }}
-          />
+          <Upload {...uploadProps}>
+            <Button
+              type="primary"
+              text={t('changePhotoButtonText')}
+              onClick={() => navigate(Route.PERSONAL_INFO)}
+              style={{ marginTop: '20px' }}
+            />
+          </Upload>
         </Col>
         <Col span={12}>
           {isEditMode ? (
