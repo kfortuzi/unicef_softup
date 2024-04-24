5dc667777aa5e2493095a4a672b9160384cfd80a
Emre KAS <emre.kas@softup.co>
2024-04-24 09:32:11 +0000

    UA-186 Fixed the questionnaire screen issues
    * fix: fixed the questionnaire screen issues
    
    Approved-by: Stivi Ndoni

--- react-app/src/components/coverLetter/CoverLetterQuestionnaireForm/CoverLetterQuestionnaireForm.tsx
+++ react-app/src/components/coverLetter/CoverLetterQuestionnaireForm/CoverLetterQuestionnaireForm.tsx
@@ -1,11 +1,13 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import usePostCoverLetterWizard from 'src/api/coverLetters/hooks/usePostCoverLetterWizard';
 import Button from 'src/components/common/Button/Button';
 import InputTextArea from 'src/components/common/InputTextArea/InputTextArea';
+import LoadingFullPage from 'src/components/common/LoadingFullPage/LoadingFullPage';
 import i18n from 'src/locales';
 
 import { defaultValues } from './constants';
@@ -14,127 +16,138 @@ import validationSchema from './validation';
 
 const CoverLetterQuestionnaireForm: React.FC = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'coverLetterDetails' });
-  const { mutate: postCoverLetterWizard } = usePostCoverLetterWizard();
+  const { mutateAsync: postCoverLetterWizardAsync } = usePostCoverLetterWizard();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const { handleSubmit, control } = useForm({
     defaultValues: defaultValues,
     resolver: yupResolver(validationSchema),
     shouldFocusError: true,
   });
 
-  const submitForm = handleSubmit((data) => {
-    postCoverLetterWizard(data);
+  const submitForm = handleSubmit(async (data) => {
+    setLoading(true);
+    postCoverLetterWizardAsync(data).then((generatedCoverLetter) => {
+      navigate(`/cover-letters/${generatedCoverLetter?.id}`, { replace: true });
+    }).finally(() => {
+      setLoading(false);
+    });
   });
 
-  return (
-    <div className="cover-letter-questionnaire-form-container">
-      <form onSubmit={submitForm}>
-        <Controller
-          control={control}
-          name={FormField.TITLE}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              error={error?.message}
-              onChange={onChange}
-              placeholder={t('title')}
-              className="input-textarea"
-              label={t('title')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.COMPANY}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              error={error?.message}
-              onChange={onChange}
-              placeholder={t('company')}
-              className="input-textarea"
-              label={t('company')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.TO_PERSON}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              error={error?.message}
-              onChange={onChange}
-              placeholder={t('to')}
-              className="input-textarea"
-              label={t('to')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.COMPANY_ADDRESS}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              error={error?.message}
-              onChange={onChange}
-              placeholder={t('companyAddress')}
-              className="input-textarea"
-              label={t('companyAddress')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.MOTIVE}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              error={error?.message}
-              onChange={onChange}
-              placeholder={t('motive')}
-              className="input-textarea"
-              label={t('motive')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.TOPIC}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              error={error?.message}
-              onChange={onChange}
-              placeholder={t('topic')}
-              className="input-textarea"
-              label={t('topic')}
-            />
-          )}
-        />
-        <div className="button-group-container">
-          <Button
-            type="primary"
-            text={i18n.t('globalStrings.submit')}
-            htmlType="submit"
-            style={{ marginTop: '20px' }}
+  if (loading) {
+    return <LoadingFullPage />;
+  } else {
+    return (
+      <div className="cover-letter-questionnaire-form-container">
+        <form onSubmit={(e) => { e.preventDefault(); submitForm(e); }}>
+          <Controller
+            control={control}
+            name={FormField.TITLE}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                error={error?.message}
+                onChange={onChange}
+                placeholder={t('title')}
+                className="input-textarea"
+                label={t('title')}
+              />
+            )}
           />
-        </div>
-      </form>
-    </div>
-  );
+          <Controller
+            control={control}
+            name={FormField.COMPANY}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                error={error?.message}
+                onChange={onChange}
+                placeholder={t('company')}
+                className="input-textarea"
+                label={t('company')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.TO_PERSON}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                error={error?.message}
+                onChange={onChange}
+                placeholder={t('to')}
+                className="input-textarea"
+                label={t('to')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.COMPANY_ADDRESS}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                error={error?.message}
+                onChange={onChange}
+                placeholder={t('companyAddress')}
+                className="input-textarea"
+                label={t('companyAddress')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.MOTIVE}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                error={error?.message}
+                onChange={onChange}
+                placeholder={t('motive')}
+                className="input-textarea"
+                label={t('motive')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.TOPIC}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                error={error?.message}
+                onChange={onChange}
+                placeholder={t('topic')}
+                className="input-textarea"
+                label={t('topic')}
+              />
+            )}
+          />
+          <div className="button-group-container">
+            <Button
+              type="primary"
+              text={i18n.t('globalStrings.submit')}
+              htmlType="submit"
+              style={{ marginTop: '20px' }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default CoverLetterQuestionnaireForm;
--- react-app/src/components/resume/ResumeQuestionnaireForm/ResumeQuestionnaireForm.tsx
+++ react-app/src/components/resume/ResumeQuestionnaireForm/ResumeQuestionnaireForm.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import usePostResumesWizard from 'src/api/resumes/hooks/usePostResumesWizard';
 import Button from 'src/components/common/Button/Button';
 import InputTextArea from 'src/components/common/InputTextArea/InputTextArea';
+import LoadingFullPage from 'src/components/common/LoadingFullPage/LoadingFullPage';
 import i18n from 'src/locales';
 import { Route } from 'src/router/enums';
 
@@ -15,7 +16,7 @@ import { FormField } from './enums';
 import validationSchema from './validations';
 
 const ResumeQuestionnaireForm: React.FC = () => {
-  const { mutateAsync: postResumesWizard, isPending } = usePostResumesWizard();
+  const { mutateAsync: postResumesWizardAsync, isPending } = usePostResumesWizard();
   const navigate = useNavigate();
 
   const { handleSubmit, control } = useForm({
@@ -24,177 +25,189 @@ const ResumeQuestionnaireForm: React.FC = () => {
     resolver: yupResolver(validationSchema),
   });
 
+  const [loading, setLoading] = useState(false);
+
   const { t } = useTranslation('translation', { keyPrefix: 'resumeQuestionnaire' });
 
   const submitForm = handleSubmit(async (data) => {
-    await postResumesWizard(data);
-    navigate(Route.MY_RESUME);
+    setLoading(true);
+    postResumesWizardAsync(data)
+      .then(() => {
+        navigate(Route.MY_RESUME);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   });
 
-  return (
-    <div className="resume-questionnaire-form-container">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          submitForm(e);
-        }}
-      >
-        <Controller
-          control={control}
-          name={FormField.EXPERIENCES}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('experiences')}
-              className="input-textarea"
-              label={t('experiences')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.EDUCATIONS}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('educations')}
-              label={t('educations')}
-              className="input-textarea"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.NATIVE_LANGUAGE}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('nativeLanguage')}
-              className="input-textarea"
-              label={t('nativeLanguage')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.OTHER_LANGUAGE}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('otherLanguage')}
-              className="input-textarea"
-              label={t('otherLanguage')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.DIGITAL_SKILLS}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('digitalSkills')}
-              className="input-textarea"
-              label={t('digitalSkills')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.TECHNICAL_SKILLS}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('technicalSkills')}
-              className="input-textarea"
-              label={t('technicalSkills')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.SOFT_SKILLS}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('softSkills')}
-              className="input-textarea"
-              label={t('softSkills')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.HOBBIES}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('hobbies')}
-              className="input-textarea"
-              label={t('hobbies')}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name={FormField.SUMMARY}
-          render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
-            <InputTextArea
-              inputRef={ref}
-              name={name}
-              value={value}
-              onChange={onChange}
-              error={error?.message}
-              placeholder={t('summary')}
-              className="input-textarea"
-              label={t('summary')}
-            />
-          )}
-        />
-        <div className="button-group-container">
-          <Button
-            type="primary"
-            text={i18n.t('globalStrings.submit')}
-            htmlType="submit"
-            style={{ marginTop: '20px' }}
-            loading={isPending}
+  if (loading) {
+    return <LoadingFullPage />;
+  } else {
+    return (
+      <div className="resume-questionnaire-form-container">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            submitForm(e);
+          }}
+        >
+          <Controller
+            control={control}
+            name={FormField.EXPERIENCES}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('experiences')}
+                className="input-textarea"
+                label={t('experiences')}
+              />
+            )}
           />
-        </div>
-      </form>
-    </div>
-  );
+          <Controller
+            control={control}
+            name={FormField.EDUCATIONS}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('educations')}
+                label={t('educations')}
+                className="input-textarea"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.NATIVE_LANGUAGE}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('nativeLanguage')}
+                className="input-textarea"
+                label={t('nativeLanguage')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.OTHER_LANGUAGE}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('otherLanguage')}
+                className="input-textarea"
+                label={t('otherLanguage')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.DIGITAL_SKILLS}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('digitalSkills')}
+                className="input-textarea"
+                label={t('digitalSkills')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.TECHNICAL_SKILLS}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('technicalSkills')}
+                className="input-textarea"
+                label={t('technicalSkills')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.SOFT_SKILLS}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('softSkills')}
+                className="input-textarea"
+                label={t('softSkills')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.HOBBIES}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('hobbies')}
+                className="input-textarea"
+                label={t('hobbies')}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name={FormField.SUMMARY}
+            render={({ field: { name, value, onChange, ref }, fieldState: { error } }) => (
+              <InputTextArea
+                inputRef={ref}
+                name={name}
+                value={value}
+                onChange={onChange}
+                error={error?.message}
+                placeholder={t('summary')}
+                className="input-textarea"
+                label={t('summary')}
+              />
+            )}
+          />
+          <div className="button-group-container">
+            <Button
+              type="primary"
+              text={i18n.t('globalStrings.submit')}
+              htmlType="submit"
+              style={{ marginTop: '20px' }}
+              loading={isPending}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default ResumeQuestionnaireForm;
