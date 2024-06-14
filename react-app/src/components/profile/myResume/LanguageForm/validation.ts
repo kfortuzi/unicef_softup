b74332cdbdbea353a154d0752f93efdfd301a0db
Kostandin <kostandin.dervishaj@softup.co>
2024-06-14 12:32:02 +0000

    Merged in bugfix/fix-validation-for-language (pull request #183)
    [BUGFIX] FE: Fix validation for language in my resume.
    
    * Minor validation fix
    
    
    Approved-by: Stivi Ndoni

--- react-app/src/components/profile/myResume/LanguageForm/validation.ts
+++ react-app/src/components/profile/myResume/LanguageForm/validation.ts
@@ -16,19 +16,22 @@ const validationSchema = object().shape({
     i18n.t(`${basePrefix}.required`, { field: i18n.t(`${sectionPrefix}.isNative`) }),
   ),
   [FormField.READING]: string().when(FormField.IS_NATIVE, {
-    is: false,
+    is: (isNative: number | boolean) => Number(isNative) === 0,
     then: (schema) =>
       schema.required(i18n.t(`${basePrefix}.required`, { field: i18n.t(`${sectionPrefix}.reading`) })),
+    otherwise: (schema) => schema.nullable(),
   }),
   [FormField.WRITING]: string().when(FormField.IS_NATIVE, {
-    is: false,
+    is: (isNative: number | boolean) => Number(isNative) === 0,
     then: (schema) =>
       schema.required(i18n.t(`${basePrefix}.required`, { field: i18n.t(`${sectionPrefix}.writing`) })),
+    otherwise: (schema) => schema.nullable(),
   }),
   [FormField.SPEAKING]: string().when(FormField.IS_NATIVE, {
-    is: false,
+    is: (isNative: number | boolean) => Number(isNative) === 0,
     then: (schema) =>
       schema.required(i18n.t(`${basePrefix}.required`, { field: i18n.t(`${sectionPrefix}.speaking`) })),
+    otherwise: (schema) => schema.nullable(),
   }),
 });
 
